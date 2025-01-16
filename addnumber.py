b2f089542fd6bfb87209e0a1f14512d8e8c0fc65
Nirmit Patel <avsk1828@gmail.com>
2025-01-16 23:15:21 +0530

    check testcase

--- addnumber.py
+++ addnumber.py
@@ -10,3 +10,23 @@ def add(numbers: str) -> int:
         numbers = numbers_part.replace(delimiter, ",")
     number_list = re.split(r'[\n,]', numbers)
     negative_numbers = [num for num in number_list if int(num) < 0]
+    if negative_numbers:
+        raise ValueError(f"Negative numbers not allowed: {', '.join(negative_numbers)}")
+    return sum(map(int, number_list))
+
+print(add(""))           
+print(add("1"))          
+print(add("1,5"))        
+print(add("1\n2,3"))     
+print(add("//;\n1;2"))   
+print(add("//|\n1|2|3")) 
+
+try:
+    print(add("//;\n1;-2"))  
+except ValueError as e:
+    print(e)  
+
+try:
+    print(add("//;\n1;-2;-3")) 
+except ValueError as e:
+    print(e) 
