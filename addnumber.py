cb1d6fea511d06059a54043e19c0efea918db239
Nirmit Patel <avsk1828@gmail.com>
2025-01-16 22:23:50 +0530

    added first functionality

---
+++ addnumber.py
@@ -0,0 +1,10 @@
+import re
+
+def add(numbers: str) -> int:
+
+    if numbers == "":
+        return 0
+    if numbers.startswith("//"):
+        delimiter_line, numbers_part = numbers.split("\n", 1)
+        delimiter = delimiter_line[2:]
+        numbers = numbers_part.replace(delimiter, ",")
